501545f10e72bbbd0aa31401ad10b9c44ca677a5
meggs1 <megan.schertz@gmail.com>
2020-11-16 15:19:47 -0600

    commit tree 5a1eabf85d9a0cfb267d309e5e55686fd7feb26e

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,32 @@
 class Customer
+    attr_accessor :name, :age
   
-end
+    @@all = []
+  
+    def initialize(name, age)
+      @name = name
+      @age = age
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+
+    def new_meal(waiter, total, tip=0)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals #returns an Array of Meal instances associated with this customer
+        Meal.all.select do |meal|
+          meal.customer == self
+        end
+    end
+
+    def waiters #returns an Array of Waiter instances associated with this customer's meals
+        meals.map do |meal|
+          meal.waiter
+        end
+    end
+  
+  end
